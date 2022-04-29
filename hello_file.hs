4db10fde3badd3973ded0ab79a636c5e65c1d221
bryan <bryanfeliciano892@gmail.com>
2022-04-29 10:25:43 -0500

    checking contents of files and printing contents

--- hello_file.hs
+++ hello_file.hs
@@ -5,13 +5,14 @@ openFile :: FilePath -> IOMode -> IO Handle
 main :: IO()
 main = do
     helloFile <- openFile "hello.txt" ReadMode
-    putStrLn firstLine 
-    secondLine <- hGetLine helloFile
-    goodByeFile <- openFile "goodBye.txt" WriteMode
-    hPutStrLn goodByeFile secondLine
-    hClose helloFile
-    hclosegoodByeFile
-    putStrLn "done!"
+    hasLine <- hIsEOF helloFile
+    firstLine <- if not hasLine
+                 then hGetLine helloFile
+                 else return "empty"
+    hasSecondLine <- hIsEOF helloFile
+    secondLine <- if not hasSecondLine
+                  then hGetLine helloFile
+                  else return " "
 
 
 -- Initial version --
@@ -19,4 +20,17 @@ main = do
 -- main = do
 --     myFile <- openFile "hello.txt" ReadMode
 --     hClose myFile
+--     putStrLn "done!"
+
+
+-- Reading from and writing to another file --
+-- main :: IO()
+-- main = do
+--     helloFile <- openFile "hello.txt" ReadMode
+--     putStrLn firstLine 
+--     secondLine <- hGetLine helloFile
+--     goodByeFile <- openFile "goodBye.txt" WriteMode
+--     hPutStrLn goodByeFile secondLine
+--     hClose helloFile
+--     hclosegoodByeFile
 --     putStrLn "done!"
