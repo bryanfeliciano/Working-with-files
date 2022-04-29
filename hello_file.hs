7b9b643a30b8c0900610e8598540c85e6d15b08e
bryan <bryanfeliciano892@gmail.com>
2022-04-29 09:47:03 -0500

    reading from a file and writing to other file

--- hello_file.hs
+++ hello_file.hs
@@ -2,6 +2,18 @@ import System.IO
 
 openFile :: FilePath -> IOMode -> IO Handle
 
+main :: IO()
+main = do
+    helloFile <- openFile "hello.txt" ReadMode
+    putStrLn firstLine 
+    secondLine <- hGetLine helloFile
+    goodByeFile <- openFile "goodBye.txt" WriteMode
+    hPutStrLn goodByeFile secondLine
+    hClose helloFile
+    hclosegoodByeFile
+    putStrLn "done!"
+
+
 -- Initial version --
 -- main :: IO()
 -- main = do
